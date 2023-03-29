8388cfe1039a255636f26c0fded486b5eeb08450
dlagunfry <collinsedim@gmail.com>
2023-03-29 13:17:53 +0100

    optimized responsive Grid for mobile devices

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function removeMobileGrid() {
+    const colsGrid = document.querySelectorAll('#mobileGrid');
+    const viewPortWidth = window.innerWidth || document.documentElement.clientWidth;
+    for (let i = 0; i < colsGrid.length; i++) {
+    if (viewPortWidth <= 768) {
+        colsGrid[i].classList.remove('col');
+        colsGrid[i].classList.add('bottomMargin');
+    } else {
+        colsGrid[i].classList.add('col');
+    }
+}
+}
+window.addEventListener('load', removeMobileGrid);
+window.addEventListener('resize', removeMobileGrid);
